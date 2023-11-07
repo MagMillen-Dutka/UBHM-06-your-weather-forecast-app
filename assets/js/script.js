56d3812544e39bee1988c17871ad211871ef3c44
Mag Millen-Dutka <145022728+MagMillen-Dutka@users.noreply.github.com>
2023-11-07 12:31:41 +0000

    Correction of HTML coding

---
+++ assets/js/script.js
@@ -0,0 +1,99 @@
+var cityInputEl = document.querySelector("#city-input");
+var searchForm = document.querySelector("#search-form");
+var clearBtn = document.querySelector("#clear-history-button");
+var pastSearchedCitiesEl = document.querySelector("#search-history");
+var todayWeather = document.querySelector("#today-weather");
+var fiveDayForecast = document.querySelector("#five-day-forecast");
+var searchHistory = [];
+
+// This will show the present day and five day future forecast
+
+function forecast(event) {
+    // This event is needed to stop page refreshing and clearing input. Do not remove.
+    event.preventDefault();
+    var cityName = cityInputEl.value;
+    displayWeather(cityName);
+}
+function displayWeather(cityName) {
+    var url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=9dd332c2cdf5ad3eee158912aa75b747&units=imperial`;
+    fetch(url)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (currentData) {
+            console.log(currentData);
+            var oneCallUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${currentData.coord.lat}&lon=${currentData.coord.lon}&appid=9dd332c2cdf5ad3eee158912aa75b747&units=imperial`;
+            fetch(oneCallUrl)
+                .then(function (response) {
+                    return response.json();
+                })
+                .then(function (fiveDayData) {
+                    if (searchHistory.includes(currentData.name) === false) {
+                        searchHistory.push(currentData.name);
+                        localStorage.setItem("city", JSON.stringify(searchHistory));
+                    }
+                    displayCity();
+                    console.log(fiveDayData);
+                    todayWeather.innerHTML = `<ul>
+        <li class="title"><b> ${currentData.name} </b> on <span> ${moment(
+                        currentData.dt,
+                        "X"
+                    ).format(" dddd Do MMMM YYYY")} </span></li>
+        <li><img src ="http://openweathermap.org/img/wn/${currentData.weather[0].icon
+                        }@2x.png" /></li>
+        <li>Temperature: <b> ${currentData.main.temp} </b></li>
+        <li>Wind Speed: <b> ${currentData.wind.speed} </b></li>
+        <li>Humidity: <b> ${currentData.main.humidity} </b></li>
+    </ul>
+        `;
+                    var cards = "";
+                    for (var i = 1; i < 6; i++) {
+                        cards =
+                            cards +
+                            `<ul class="col-12 col-xl-2 day">
+        <li>${moment(fiveDayData.daily[i].dt, "X").format(" MM/DD/YYYY")}</li>
+        <li><img src ="http://openweathermap.org/img/wn/${fiveDayData.daily[i].weather[0].icon
+                            }@2x.png" /></li>
+        <li>Temperature: <b> ${fiveDayData.daily[i].temp.day} </b></li>
+        <li>Wind Speed: <b> ${fiveDayData.daily[i].wind_speed} </b></li>
+        <li>Humidity: <b> ${fiveDayData.daily[i].humidity}</b></li>
+    </ul>`;
+                    }
+                    fiveDayForecast.innerHTML = cards;
+                });
+        });
+}
+function displayCity() {
+    if (localStorage.getItem("city")) {
+        searchHistory = JSON.parse(localStorage.getItem("city"));
+    }
+    var cityList = "";
+    for (var i = 0; i < searchHistory.length; i++) {
+        cityList =
+            cityList +
+            `<button class="btn btn-secondary my-2" type="submit">${searchHistory[i]}</button>`;
+    }
+    pastSearchedCitiesEl.innerHTML = cityList;
+    var myDashTwo = document.querySelectorAll(".my-2");
+    for (var i = 0; i < myDashTwo.length; i++) {
+        myDashTwo[i].addEventListener("click", function () {
+            displayWeather(this.textContent);
+        });
+    }
+}
+displayCity();
+
+searchForm.addEventListener("submit", forecast); 
+
+function clearSearchHistory() {
+    localStorage.clear();
+    pastSearchedCitiesEl.innerHTML = "";
+    searchHistory = [];
+}
+clearBtn.addEventListener("click", function () {
+    clearSearchHistory();
+});
+
+// if the clear button is clicked, search history buttons are removed and local storage cleared
+// localStorage.clear();
+// location.reload();
